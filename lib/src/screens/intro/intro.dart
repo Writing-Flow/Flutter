59699b99815ece1a1f6ac53b795520dcfe903e64
eJwtzD1vwyAUheH/wuwBxx8xbOnQoR+qKnXqgq7hOiYGQy6maRTlv9eNvB0d6XlvDLTGlL7ChDOTjO9rMEJUZdnu+L4zDUI1DJUWvBEdL/umGdqu7jgrWKTwYw3SRm9bSS1b6jgG5ceX8XyK41v2rciv7y0c5s98Pnz43Yl/19WzfaI19SBquUZcXY9A+P8mHR4HGG9nGXPvrFYTXgtCMDLQcR0xFDkhSfRgXXEJNA0uXFaMv9ESJgULk3N27n7/A+NnT5U= <kshyunn12@gmail.com>
2024-07-24 20:57:30 +0900

    Design : Add the lost image file, edit textStyle in intro, and edit image padding value in dictionary search

--- lib/src/screens/intro/intro.dart
+++ lib/src/screens/intro/intro.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import 'login.dart';
@@ -16,57 +17,59 @@ class _IntroState extends State<Intro> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children: [
-            // 로고&슬로건 영역
-            Expanded(
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      // 로고
-                      Image.asset('asset/images/intro/intro_logo.png', width: 150, height: 197),
-                      // 슬로건
-                      SizedBox(height: 15),
+      children: [
+        // 로고&슬로건 영역
+        Expanded(
+            child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // 로고
+              Image.asset('asset/images/intro/intro_logo.png',
+                  width: 150, height: 197),
+              // 슬로건
+              SizedBox(height: 15),
 
-                      const Text('머리 속에 떠돌던 생각을\n 작성해보시겠습니까?', textAlign: TextAlign.center)
-                    ],
-                  ),
-                )),
-            // 가입, 로그인 버튼 영역
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
+              const Text(
+                '머리 속에 떠돌던 생각을\n 작성해보시겠습니까?',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        )),
+        // 가입, 로그인 버튼 영역
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  // 회원가입
+                  ElevatedButton(
+                      onPressed: () {
+                        //Get.to(() => const Register());
+                      },
+                      child: const Text('가입하기')),
+                  // 로그인
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // 회원가입
-                      ElevatedButton(
-                          onPressed: (){
-                            //Get.to(() => const Register());
+                      const Text('이미 가입하셨나요?'),
+                      TextButton(
+                          onPressed: () {
+                            Get.to(() => const Login());
                           },
-                          child: const Text('가입하기')
-                      ),
-                      // 로그인
-                      const SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('이미 가입하셨나요?'),
-                          TextButton(
-                              onPressed: (){
-                                Get.to(() => const Login());
-                              },
-                              child: const Text('로그인')
-                          )
-                        ],
-                      )
+                          child: const Text('로그인'))
                     ],
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             )
           ],
         )
-    );
+      ],
+    ));
   }
 }
