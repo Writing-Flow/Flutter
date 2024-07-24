d96c59851cbcce0d01a13a8a22da2974a8ea8305
eJwtzD1vwyAUheH/wuwBxx8xbOnQoR+qKnXqgq7hOiYGQy6maRTlv9eNvB0d6XlvDLTGlL7ChDOTjO9rMEJUZdnu+L4zDUI1DJUWvBEdL/umGdqu7jgrWKTwYw3SRm9bSS1b6jgG5ceX8XyK41v2rciv7y0c5s98Pnz43Yl/19WzfaI19SBquUZcXY9A+P8mHR4HGG9nGXPvrFYTXgtCMDLQcR0xFDkhSfRgXXEJNA0uXFaMv9ESJgULk3N27n7/A+NnT5U= <kshyunn12@gmail.com>
2024-07-24 17:07:12 +0900

    Feat : add image in dictionary search screen

--- lib/src/screens/home/dictionary/dictionary_search.dart
+++ lib/src/screens/home/dictionary/dictionary_search.dart
@@ -14,25 +14,30 @@ class DictionarySearch extends StatefulWidget {
 
 class _DictionarySearchState extends State<DictionarySearch> {
   final dictionaryController = Get.put(DictionaryController());
+  bool visible = true;
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.search),
           onPressed: () async {
-
-            Map json =  await DictionaryProvider().search(dictionaryController.searchTextController.text); // 검색
-            if(json.isNotEmpty){
-              List<DictionarySearchModel> tmp = json['data'].map<DictionarySearchModel>((m) =>
-                  DictionarySearchModel.parse(m)).toList();
+            Map json = await DictionaryProvider()
+                .search(dictionaryController.searchTextController.text); // 검색
+            if (json.isNotEmpty) {
+              List<DictionarySearchModel> tmp = json['data']
+                  .map<DictionarySearchModel>(
+                      (m) => DictionarySearchModel.parse(m))
+                  .toList();
 
               dictionaryController.searchResultList.clear();
               dictionaryController.searchResultList.assignAll(tmp);
             }
 
-            await dictionaryController.addSearchHistory();  // 검색 기록
+            await dictionaryController.addSearchHistory(); // 검색 기록
           },
         ),
         title: TextField(
@@ -46,38 +51,58 @@ class _DictionarySearchState extends State<DictionarySearch> {
             // 이 경우는 상태를 변경하는 로직을 추가할 수 있습니다.
           },
         ),
-
       ),
-
       body: Column(
         children: [
           SizedBox(
             height: 40,
-            child: Obx(() =>
-              ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: dictionaryController.categoryButtonList.length,
-                itemBuilder: (context, index) {
-                  return dictionaryController.categoryButtonList[index];
-                }
-              ),
+            child: Obx(
+              () => ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: dictionaryController.categoryButtonList.length,
+                  itemBuilder: (context, index) {
+                    return dictionaryController.categoryButtonList[index];
+                  }),
             ),
           ),
           // 검색 결과 리스트
           Expanded(
-            child: Obx(() =>
-              ListView.builder(
-                scrollDirection: Axis.vertical,
-                itemCount: dictionaryController.searchResultList.length,
-                itemBuilder: (context, index){
-                  return DictionarySearchListViewItem(dictionaryController.searchResultList[index]);
-                },
-              )
-            )
-          )
+              child: Stack(
+            children: [
+              Obx(() {
+                visible = dictionaryController.searchResultList.isEmpty;
+                return Visibility(
+                  visible: visible,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Image.asset(
+                          "asset/images/dictionary/dictionary_intro.png",
+                          height: 140,
+                          width: screenWidth - 40, // 화면 너비에서 양옆 20씩 패딩
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+              // 검색 결과 리스트
+              Obx(() {
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: dictionaryController.searchResultList.length,
+                  itemBuilder: (context, index) {
+                    return DictionarySearchListViewItem(
+                        dictionaryController.searchResultList[index]);
+                  },
+                );
+              }),
+            ],
+          ))
         ],
       ),
     );
   }
 }
-
